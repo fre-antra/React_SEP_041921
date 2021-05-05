1e7baed2689e09f9a1990c233e52165b3ac4b783
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-05-04 22:33:25 -0400

    exam 1

---
+++ Week3/Exam1/index.js
@@ -0,0 +1,46 @@
+const itunesAPI = (ARTIST_NAME) => {
+  const baseUrl = `https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=500`;
+
+  const getdata = fetchJsonp(baseUrl).then((res) => res.json());
+
+  return getdata;
+};
+
+const addList = (data, query) => {
+  const grid = document.querySelector(".grid");
+  const info = document.querySelector(".content-info");
+
+  let listArr = data.map((data) => {
+    return `
+           <div class="card">
+             <img class="card-image" src="${data.artworkUrl100}">
+             <div class="card-name">${data.collectionCensoredName}</div>
+           </div>
+         `;
+  });
+
+  info.innerHTML = `${data.length} results for "${query}"`;
+  grid.innerHTML = listArr.join("");
+};
+
+const searchEventListeners = (() => {
+  const searchbar = document.querySelector(".searchbar");
+  const container = document.querySelector(".search-container");
+
+  let state = {
+    text: "",
+  };
+
+  searchbar.addEventListener("keyup", (e) => {
+    state.text = e.target.value;
+  });
+
+  container.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    itunesAPI(state.text).then((data) => {
+      addList(data.results, state.text);
+    });
+  });
+})();
+
