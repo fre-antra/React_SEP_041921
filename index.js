b8c0d097a332aba487b2598926001fd4e5d0fce3
Allen <allenlane900@gmail.com>
2021-05-17 12:56:21 -0400

    123

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hello")
+// function Parent() {
+//     this.name = 'parent'
+// }
+
+// Parent.prototype.getName = function () {
+//     console.log(this.name);
+// }
+
+// function Child() {
+
+// }
+
+// Child.prototype = new Parent();
+// var child = new Child();
+// console.log(child.name)
+
+function Parent(name) {
+    this.name = name;
+}
+Parent.prototype.getName = function () {
+    console.log(this.name, this.age);
+}
+function Child(name, age) {
+    Parent.call(this, name);
+    this.age = age;
+}
+var F = function () { }
+F.prototype = Parent.prototype;
+Child.prototype = new F();
+var child1 = new Child("cc", 20)
+console.log(child1)　//Child {name: "cc", age: 20}
