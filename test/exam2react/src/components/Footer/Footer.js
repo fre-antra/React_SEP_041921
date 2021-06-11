3cb67fb883b01d0e62abdf7a688856fdbb55a4f0
JustCodeandChill <inforreceiver@gmail.com>
2021-06-10 19:42:59 -0500

    add responsiveness to Footer

--- test/exam2react/src/components/Footer/Footer.js
+++ test/exam2react/src/components/Footer/Footer.js
@@ -13,70 +13,84 @@ const Contact = () => {
       <div className="footer-section">
         <CenterDiv>
           <div className="mt-50"></div>
-          <Grid columns={8} centered>
-            <Grid.Row>
-              <Grid.Column>
-                <img className="icon" src={fbLogo} alt="logo" />
-              </Grid.Column>
-              <Grid.Column>
-                <img className="icon" src={liLogo} alt="logo" />
-              </Grid.Column>
-              <Grid.Column>
-                <img className="icon" src={pinLogo} alt="logo" />
-              </Grid.Column>
-              <Grid.Column>
-                <img className="icon" src={emailLogo} alt="logo" />
-              </Grid.Column>
-            </Grid.Row>
+          <Grid centered>
+            <CenterDiv>
+              <Grid.Row>
+                <Grid.Column computer={4} mobile={16}>
+                  <img className="icon" src={fbLogo} alt="logo" />
+                </Grid.Column>
+                <Grid.Column computer={4} mobile={16}>
+                  <img className="icon" src={liLogo} alt="logo" />
+                </Grid.Column>
+                <Grid.Column computer={4} mobile={16}>
+                  <img className="icon" src={pinLogo} alt="logo" />
+                </Grid.Column>
+                <Grid.Column computer={4} mobile={16}>
+                  <img className="icon" src={emailLogo} alt="logo" />
+                </Grid.Column>
+              </Grid.Row>
+            </CenterDiv>
 
-            <Grid.Row columns={2} centered>
-              <Grid.Column>
-                <p style={{ textAlign: "right" }}>Phone: XXXXXX</p>
-              </Grid.Column>
-              <Grid.Column>
-                <p>Email: XXXXX@gmail.com</p>
-              </Grid.Column>
+            <Grid.Row>
+              <Grid centered>
+                <Grid.Column computer={8} tablet={8} mobile={8}>
+                  <p>Phone: XXXXXX</p>
+                </Grid.Column>
+                <Grid.Column computer={8} tablet={8} mobile={8}>
+                  <p>Email: XXXXX@gmail.com</p>
+                </Grid.Column>
+              </Grid>
             </Grid.Row>
 
             <Grid.Row centered columns={5}>
-              <Grid.Column>
-                <div className="footer-section__list">
-                  <a className="footer-section__list-header">XXXXXX</a>
-                  <a href="#">Copyright © 2021</a>
-                  <a href="#">NY</a>
-                </div>
-              </Grid.Column>
-              <Grid.Column>
-                <div className="footer-section__list">
-                  <a href="#home" className="footer-section__list-header">Home</a>
-                </div>
-              </Grid.Column>
-              <Grid.Column>
-                <div className="footer-section__list">
-                  <a href="#portfolio" className="footer-section__list-header">
-                    Portfolio
-                  </a>
-                  <a href="#">Project 1</a>
-                  <a href="#">Project 2</a>
-                  <a href="#">Project 3</a>
-                  <a href="#">See All</a>
-                </div>
-              </Grid.Column>
-              <Grid.Column>
-                <div className="footer-section__list">
-                  <a href="#portfolio" className="footer-section__list-header">
-                    Resume
-                  </a>
-                  <a href="#home">Download</a>
-                </div>
-              </Grid.Column>
-              <Grid.Column>
-                <div className="footer-section__list">
-                  <a href="#contact" className="footer-section__list-header">
-                    Contact
-                  </a>
-                </div>
-              </Grid.Column>
+              <CenterDiv>
+                <Grid.Column>
+                  <div className="footer-section__list">
+                    <a className="footer-section__list-header">XXXXXX</a>
+                    <a href="#">Copyright © 2021</a>
+                    <a href="#">NY</a>
+                  </div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div className="footer-section__list">
+                    <a href="#home" className="footer-section__list-header">
+                      Home
+                    </a>
+                  </div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div className="footer-section__list">
+                    <a
+                      href="#portfolio"
+                      className="footer-section__list-header"
+                    >
+                      Portfolio
+                    </a>
+                    <a href="#">Project 1</a>
+                    <a href="#">Project 2</a>
+                    <a href="#">Project 3</a>
+                    <a href="#">See All</a>
+                  </div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div className="footer-section__list">
+                    <a
+                      href="#portfolio"
+                      className="footer-section__list-header"
+                    >
+                      Resume
+                    </a>
+                    <a href="#home">Download</a>
+                  </div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div className="footer-section__list">
+                    <a href="#contact" className="footer-section__list-header">
+                      Contact
+                    </a>
+                  </div>
+                </Grid.Column>
+              </CenterDiv>
             </Grid.Row>
           </Grid>
         </CenterDiv>
