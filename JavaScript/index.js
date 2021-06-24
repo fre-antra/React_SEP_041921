72403046d24f61fbbf4310947ed245dae3ab005d
show3567 <ydong011@fiu.edu>
2021-06-24 13:41:06 -0400

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,154 +1,154 @@
-class MyPromise {
-    thenCbQueue = [];
-    catchCbQueue = [];
-    promiseState = "pending";
-    curData;
+// class MyPromise {
+//     thenCbQueue = [];
+//     catchCbQueue = [];
+//     promiseState = "pending";
+//     curData;
 
-    constructor(executor) {
-        try {
-            executor(this.resolve, this.reject.bind(this));
-        } catch (error) {
-            this.reject(error);
-        }
-    }
+//     constructor(executor) {
+//         try {
+//             executor(this.resolve, this.reject.bind(this));
+//         } catch (error) {
+//             this.reject(error);
+//         }
+//     }
 
-    resolve = (data) => {
-        setTimeout(() => {
-            try {
-                if (this.promiseState !== 'pending') return;
+//     resolve = (data) => {
+//         setTimeout(() => {
+//             try {
+//                 if (this.promiseState !== 'pending') return;
 
-                this.promiseState = 'fulfilled';
-                this.curData = data;
-                while (this.thenCbQueue.length) {
-                    const curThenCb = this.thenCbQueue.shift();
-                    if (this.curData instanceof MyPromise) {
-                        this.curData.then((promiseData) => {
-                            curThenCb(promiseData);
-                        });
-                    } else {
-                        this.curData = curThenCb(this.curData);
-                    }
-                }
-            } catch (error) {
-                this.reject(error);
-            }
-        }, 0);
-    }
+//                 this.promiseState = 'fulfilled';
+//                 this.curData = data;
+//                 while (this.thenCbQueue.length) {
+//                     const curThenCb = this.thenCbQueue.shift();
+//                     if (this.curData instanceof MyPromise) {
+//                         this.curData.then((promiseData) => {
+//                             curThenCb(promiseData);
+//                         });
+//                     } else {
+//                         this.curData = curThenCb(this.curData);
+//                     }
+//                 }
+//             } catch (error) {
+//                 this.reject(error);
+//             }
+//         }, 0);
+//     }
 
-    reject(data) {
-        this.promiseState = 'rejected';
-        setTimeout(() => {
-            this.curData = data;
-            const curCatch = this.catchCbQueue.shift();
-            if (this.curData instanceof MyPromise) {
-                this.curData.then((promiseData) => {
-                    this.curData = curCatch(promiseData);
-                });
-            } else {
-                this.curData = curCatch(this.curData);
-            }
-        }, 0);
-    }
+//     reject(data) {
+//         this.promiseState = 'rejected';
+//         setTimeout(() => {
+//             this.curData = data;
+//             const curCatch = this.catchCbQueue.shift();
+//             if (this.curData instanceof MyPromise) {
+//                 this.curData.then((promiseData) => {
+//                     this.curData = curCatch(promiseData);
+//                 });
+//             } else {
+//                 this.curData = curCatch(this.curData);
+//             }
+//         }, 0);
+//     }
 
-    then(thenCb) {
-        if (this.promiseState === 'pending') {
-            this.thenCbQueue.push(thenCb);
-        }
-        return this;
-    }
+//     then(thenCb) {
+//         if (this.promiseState === 'pending') {
+//             this.thenCbQueue.push(thenCb);
+//         }
+//         return this;
+//     }
 
-    catch(catchCb) {
-        this.catchCbQueue.push(catchCb);
-        return this;
-    }
+//     catch(catchCb) {
+//         this.catchCbQueue.push(catchCb);
+//         return this;
+//     }
 
-    static resolve(resdata) {
-        return new MyPromise((res, _) => {
-            res(resdata);
-        });
-    }
+//     static resolve(resdata) {
+//         return new MyPromise((res, _) => {
+//             res(resdata);
+//         });
+//     }
 
-    static reject(rejdata) {
-        return new MyPromise((_, rej) => {
-            rej(rejdata);
-        });
-    }
+//     static reject(rejdata) {
+//         return new MyPromise((_, rej) => {
+//             rej(rejdata);
+//         });
+//     }
 
-    static all(array) {
-        let count = 0;
-        const resolveData = new Array(array.length);
+//     static all(array) {
+//         let count = 0;
+//         const resolveData = new Array(array.length);
 
-        return new MyPromise((resolve, reject) => {
-            array.forEach((ele, i) => {
-                if (ele instanceof MyPromise) {
+//         return new MyPromise((resolve, reject) => {
+//             array.forEach((ele, i) => {
+//                 if (ele instanceof MyPromise) {
 
-                    ele.then(data => {
-                        count++;
-                        resolveData[i] = data;
-                        if (count === array.length) {
-                            resolve(resolveData);
-                        }
-                    }).catch(err => reject(err));
-                } else {
-                    count++;
-                    resolveData[i] = ele;
-                    if (count === array.length) {
-                        resolve(resolveData);
-                    }
-                }
-            });
-        });
-    }
-}
+//                     ele.then(data => {
+//                         count++;
+//                         resolveData[i] = data;
+//                         if (count === array.length) {
+//                             resolve(resolveData);
+//                         }
+//                     }).catch(err => reject(err));
+//                 } else {
+//                     count++;
+//                     resolveData[i] = ele;
+//                     if (count === array.length) {
+//                         resolve(resolveData);
+//                     }
+//                 }
+//             });
+//         });
+//     }
+// }
 
-const promise1 = MyPromise.resolve(3);
-const promise2 = 42;
-const promise3 = new MyPromise((resolve, reject) => {
-    setTimeout(resolve, 100, 'foo');
-});
+// const promise1 = MyPromise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new MyPromise((resolve, reject) => {
+//     setTimeout(resolve, 100, 'foo');
+// });
 
-MyPromise.all([promise1, promise2, promise3]).then((values) => {
-    console.log(values);
-});
-// expected output: Array [3, 42, "foo"]
+// MyPromise.all([promise1, promise2, promise3]).then((values) => {
+//     console.log(values);
+// });
+// // expected output: Array [3, 42, "foo"]
 
-const getRandomTime = () => {
-    return Math.floor(Math.random() * 5);
-}
+// const getRandomTime = () => {
+//     return Math.floor(Math.random() * 5);
+// }
 
-const p = new Promise((resolve, reject) => {
-    const timer = getRandomTime();
-    console.log(`Wait ${timer}s`);
-    if (timer > 2) {
-        setTimeout(() => {
-            resolve("Hello");
-        }, timer * 1000);
-    } else {
-        reject('timer smaller then 2');
-    }
+// const p = new Promise((resolve, reject) => {
+//     const timer = getRandomTime();
+//     console.log(`Wait ${timer}s`);
+//     if (timer > 2) {
+//         setTimeout(() => {
+//             resolve("Hello");
+//         }, timer * 1000);
+//     } else {
+//         reject('timer smaller then 2');
+//     }
 
-})
-    .then((data1) => {
-        console.log("Data1: ", data1);
-        return new MyPromise((res, rej) => {
-            try {
-                res('hello');
-            } catch (error) {
-                rej(error);
-            }
-        });
-    })
-    .then((data2) => {
-        console.log("Data2:", data2);
-    })
-    .catch(err => console.log(err));
+// })
+//     .then((data1) => {
+//         console.log("Data1: ", data1);
+//         return new MyPromise((res, rej) => {
+//             try {
+//                 res('hello');
+//             } catch (error) {
+//                 rej(error);
+//             }
+//         });
+//     })
+//     .then((data2) => {
+//         console.log("Data2:", data2);
+//     })
+//     .catch(err => console.log(err));
 
 
-async function foo() {
+// async function foo() {
 
-    const data = await fetch(url);
+//     const data = await fetch(url);
 
-}
+// }
 
 // // myFetch
 // function myFetch(url, options) {
@@ -325,12 +325,10 @@ async function foo() {
 
 // console.log([1, 2, 3].myFilter(ele => ele === 2)) ; 
 
-
-
-for (var i = 0; i < 5; i++) {
-    (function foo(v){
-        setTimeout(() => console.log(v), v * 1000);
-    })(i);
-}
-
+// for (var i = 0; i < 5; i++) {
+//     (function foo(v){
+//         setTimeout(() => console.log(v), v * 1000);
+//     })(i);
+// }
 // call stack
+
